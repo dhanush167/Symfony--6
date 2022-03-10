806cba213957009e0989ea36b51737d544637c0c
dhanushka <bydhanushka@gmail.com>
2022-03-10 21:03:28 +0530

    16.fetch data from database (first way)

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class MovieController extends AbstractController
 {
     #[Route('/movie', name: 'app_movie', methods: ['GET','HEAD'])]
-    public function index(): Response
+    public function index(MovieRepository $movieRepository): Response
     {
-        $cars = array("Volvo", "BMW", "Toyota");
-        return  $this->render('index.html.twig',array(
-            'cars'=>$cars
-        ));
+        $movies = $movieRepository->findAll();
+
+        dd($movies);
+
+//        return  $this->render('index.html.twig',array(
+//            'cars'=>$cars
+//        ));
     }
 }
