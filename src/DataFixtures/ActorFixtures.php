acf4b33b161a272527724a63552f519139cfadca
dhanushka <bydhanushka@gmail.com>
2022-03-13 10:44:02 +0530

    16.Actor database seeds (symfony console doctrine:fixtures:load)

---
+++ src/DataFixtures/ActorFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Actor;
+use App\Entity\Movie;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ActorFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        //$manager->flush();
+        $actor = new Actor();
+        $actor->setName('John Deo');
+        $manager->persist($actor);
+
+        $actor_1 = new Actor();
+        $actor_1->setName('Steve Jobs');
+        $manager->persist($actor_1);
+
+        $actor_2 = new Actor();
+        $actor_2->setName('Robert Bale');
+        $manager->persist($actor_2);
+
+        $manager->flush();
+
+    }
+}
