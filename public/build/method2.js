6e153c0d17a4ea6ef6d442d23b3dda30e014a2fc
dhanushka <bydhanushka@gmail.com>
2022-03-27 11:02:26 +0530

    38.add public folder style files

---
+++ public/build/method2.js
@@ -0,0 +1,18 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["method2"],{
+
+/***/ "./assets/javascript/method2.js":
+/*!**************************************!*\
+  !*** ./assets/javascript/method2.js ***!
+  \**************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./assets/javascript/method2.js"));
+/******/ }
+]);
